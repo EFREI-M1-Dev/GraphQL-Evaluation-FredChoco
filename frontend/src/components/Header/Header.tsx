ae765711b71b69769d386959c360a427d82daf8b
Fredray-21 <dabadie.frederic@gmail.com>
2024-05-12 22:34:06 +0200

    feat(Header) : ajout des btn Auth

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import styles from "./_Header.module.scss";
 import Logo from "../Logo/Logo";
 import Button from "../Button/Button";
+import {Link} from "react-router-dom";
 
-const Header = (
-    // props:
-    //     {
-    //         className?: string;
-    //     }
-) => {
+const Header = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.search}>
-                <Logo size={"small"} glow={true}/>
+            <div className={styles.section}>
+                <Link to={"/"} className={styles.link}>
+                    <Logo size={"small"} glow={true}/>
+                </Link>
                 <Button style={"header"} route={"search"}> [SEARCH] </Button>
                 <Button style={"header"} route={"random"}> [RANDOM] </Button>
+            </div>
+            <div className={styles.section}>
+                <Button style={"header"} route={"login"}> [LOGIN] </Button>
+                <Button style={"header"} route={"register"}> [SIGN UP] </Button>
             </div>
         </div>
     )
