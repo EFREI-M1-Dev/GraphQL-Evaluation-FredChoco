d3cba3584b8d6418521fa4a2522f9cea78cf7ad2
Chocolatiines <pimooshwoom@gmail.com>
2024-05-13 21:51:22 +0200

    fix(front): ajout lignes horizontales Comment

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -5,13 +5,16 @@ const Comment = () => {
 
     return (
         <div className={styles.container}>
+            <hr />
             <div className={styles.section}>
+
                 <p><Link to={"/"} className={styles.link}>@Fredray21</Link></p>
                 <p className={"glow"}>Il y a 2 jours</p>
             </div>
             <div className={styles.section}>
                 <p>The mv is literally just "Just go and have fun and we'll record" and i LOVE it</p>
             </div>
+            <hr />
         </div>
     )
 }
