bbc5a8c97e8d3ce7fe861a70abc0aa8705a1edbb
Chocolatiines <pimooshwoom@gmail.com>
2024-05-13 21:48:36 +0200

    feat(front): ajout Comment

---
+++ frontend/src/components/Comment/Comment.tsx
@@ -0,0 +1,18 @@
+import styles from "./_Comment.module.scss";
+import {Link} from "react-router-dom";
+
+const Comment = () => {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.section}>
+                <p><Link to={"/"} className={styles.link}>@Fredray21</Link></p>
+                <p className={"glow"}>Il y a 2 jours</p>
+            </div>
+            <div className={styles.section}>
+                <p>The mv is literally just "Just go and have fun and we'll record" and i LOVE it</p>
+            </div>
+        </div>
+    )
+}
+export default Comment;
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import styles from "./_Home.module.scss";
 import Button from "../../components/Button/Button";
+import Comment from "../../components/Comment/Comment.tsx";
 // import {UseMainControllerReturnType} from "../../controller/controllerMain";
 
 const HomePage = (
@@ -12,6 +13,7 @@ const HomePage = (
     return (
             <div className={styles.container}>
                 <Button style={"primary"} route={"test"} > Hello World </Button>
+                <Comment/>
             </div>
         );
 };
