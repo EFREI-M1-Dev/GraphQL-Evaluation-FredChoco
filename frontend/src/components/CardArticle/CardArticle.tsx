c83231b90b60f3d8a47a9c6b38521bc85879c0c7
Fredray-21 <dabadie.frederic@gmail.com>
2024-05-16 00:44:11 +0200

    feat(cardArticle) : add onClick test

--- frontend/src/components/CardArticle/CardArticle.tsx
+++ frontend/src/components/CardArticle/CardArticle.tsx
@@ -7,7 +7,10 @@ const CardArticle = (
 ) => {
 
     return (
-        <div className={styles.container+" "+(props.className || '')}>
+        <div
+            className={styles.container+" "+(props.className || '')}
+            onClick={() => console.log("cardClicked")}
+        >
             <div className={styles.section}>
                 <h2>AESPA - KARINA DANS LA SAUCE</h2>
             </div>
