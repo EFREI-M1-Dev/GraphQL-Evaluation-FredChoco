97a47e40f37aeb8239813fbc91a3d4c5b30aeefb
Fredray-21 <dabadie.frederic@gmail.com>
2024-05-21 13:06:52 +0200

    feat(footer): ajout du lien du dernier commit

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,22 @@
 import styles from "./_Footer.module.scss";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const Footer = () => {
+    const [commitSha, setCommitSha] = useState<string>("");
+
+    useEffect(() => {
+        const fetchCommitSha = async () => {
+            try {
+                const response = await fetch("https://api.github.com/repos/EFREI-M1-Dev/GraphQL-Evaluation-FredChoco/commits/main ");
+                const data = await response.json();
+                setCommitSha(data.sha);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchCommitSha().then().catch((error) => console.error(error));
+    }, []);
 
     return (
         <div className={"glow"+" "+styles.container}>
@@ -14,9 +29,9 @@ const Footer = () => {
                     <Link to={"https://github.com/Fredray-21"} target="_blank" rel="noopener noreferrer" className={styles.link}> Fredray21 </Link>
                     &
                     <Link to={"https://github.com/Chocolatiines"} target="_blank" rel="noopener noreferrer" className={styles.link}> Chocolatiines </Link>
-                    | Commit: 2024-06-01
+                    | Commit:
+                    <Link to={`https://github.com/EFREI-M1-Dev/GraphQL-Evaluation-FredChoco/commit/${commitSha}`} target="_blank" rel="noopener noreferrer" className={styles.link}> {commitSha.slice(0, 10)+"..."} </Link>
                 </p>
-
             </div>
         </div>
     )
